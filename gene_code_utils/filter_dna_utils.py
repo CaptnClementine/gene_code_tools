d279d38d39ff3decd77135817ad8d041bc4ae0ba
CaptnClementine <131146976+CaptnClementine@users.noreply.github.com>
2023-10-06 22:12:14 +0300

    Add 'is_dna' function to check input sequence

--- gene_code_utils/filter_dna_utils.py
+++ gene_code_utils/filter_dna_utils.py
@@ -1,1 +1,13 @@
+def is_dna(seq: str) -> bool:
+    """
+    Check if the input sequence consists only of DNA characters.
 
+    Args:
+        seq (str): The input DNA sequence.
+
+    Returns:
+        bool: True if the sequence contains only DNA characters, False otherwise.
+    """
+    unique_chars = set(seq)
+    nucleotides = set('ATGCatgc')
+    return unique_chars <= nucleotides
