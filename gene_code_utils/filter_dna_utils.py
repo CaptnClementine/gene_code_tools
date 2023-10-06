21654784e8f03fbbfb2e47d16e2f50be9d8a160b
CaptnClementine <131146976+CaptnClementine@users.noreply.github.com>
2023-10-06 22:13:28 +0300

    Add 'count_gc_content' function

--- gene_code_utils/filter_dna_utils.py
+++ gene_code_utils/filter_dna_utils.py
@@ -11,3 +11,20 @@ def is_dna(seq: str) -> bool:
     unique_chars = set(seq)
     nucleotides = set('ATGCatgc')
     return unique_chars <= nucleotides
+
+
+def count_gc_content(dna: str) -> float:
+    """
+    Calculate the GC content percentage of a DNA sequence.
+
+    Args:
+        dna (str): The input DNA sequence.
+
+    Returns:
+        float: The GC content percentage.
+    """
+    dna = dna.upper()
+    gc = dna.count('G') + dna.count('C')
+    at = dna.count('A') + dna.count('T')
+    gc_content = gc / (at + gc) * 100
+    return gc_content
