9cacb05d9316bc88375dfb40469ba15b6d23f35d
CaptnClementine <131146976+CaptnClementine@users.noreply.github.com>
2023-10-07 19:20:01 +0300

    Add 'is_rna' function to check if a sequence is RNA

--- gene_code_utils/dna_rna_tools_utils.py
+++ gene_code_utils/dna_rna_tools_utils.py
@@ -1,3 +1,7 @@
+DNA = set ('ATGCatgc')
+RNA = set ('AUGCaugc')
+
+
 def is_dna(seq: str) -> bool:
     """
     Check if a sequence is DNA.
@@ -10,3 +14,17 @@ def is_dna(seq: str) -> bool:
     """
     unique_chars = set(seq)
     return unique_chars <= DNA
+
+
+def is_rna(seq: str) -> bool:
+    """
+    Check if a sequence is RNA.
+
+    Args:
+        seq (str): The input sequence to check.
+
+    Returns:
+        bool: True if the sequence is RNA, False otherwise.
+    """
+    unique_chars = set(seq)
+    return unique_chars <= RNA
