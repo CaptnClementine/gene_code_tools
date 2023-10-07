9690bbd6d741caa2fed179a130747e1a0c7cdf10
CaptnClementine <131146976+CaptnClementine@users.noreply.github.com>
2023-10-07 19:19:11 +0300

    Add 'is_dna' function to check if a sequence is DNA

--- gene_code_utils/dna_rna_tools_utils.py
+++ gene_code_utils/dna_rna_tools_utils.py
@@ -1,1 +1,12 @@
+def is_dna(seq: str) -> bool:
+    """
+    Check if a sequence is DNA.
 
+    Args:
+        seq (str): The input sequence to check.
+
+    Returns:
+        bool: True if the sequence is DNA, False otherwise.
+    """
+    unique_chars = set(seq)
+    return unique_chars <= DNA
