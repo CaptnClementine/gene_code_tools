bed0dcc9fda8eebc48236d0528ec34a8729a5025
CaptnClementine <131146976+CaptnClementine@users.noreply.github.com>
2023-10-07 19:24:42 +0300

    Add 'complement' function to find complement of sequence

--- gene_code_utils/dna_rna_tools_utils.py
+++ gene_code_utils/dna_rna_tools_utils.py
@@ -41,3 +41,25 @@ def reverse(seq: str) -> str:
         str: The reversed sequence.
     """
     return seq[::-1]
+
+
+def complement(seq: str) -> str:
+    """
+    Find the complement of a DNA or RNA sequence.
+
+    Args:
+        seq (str): The input DNA or RNA sequence.
+
+    Returns:
+        str: The complemented sequence.
+    """
+    new_seq = []
+    if is_dna(seq):
+        complement_dict = {'A': 'T', 'a': 't', 'C': 'G', 'c': 'g', 'G': 'C', 'g': 'c', 'T': 'A', 't': 'a'}
+    elif is_rna(seq):
+        complement_dict = {'A': 'U', 'a': 'u', 'C': 'G', 'c': 'g', 'G': 'C', 'g': 'c', 'U': 'A', 'u': 'a'}
+    else:
+        raise ValueError("Input sequence must be DNA or RNA.")
+    for i in seq:
+        new_seq.append(complement_dict.get(i))
+    return ''.join(new_seq)
