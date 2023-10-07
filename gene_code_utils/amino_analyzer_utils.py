7a35ab56b017df9bdc2c6fe40cf2dc588c738ecb
CaptnClementine <131146976+CaptnClementine@users.noreply.github.com>
2023-10-07 16:07:34 +0300

    Add 'is_aa' function to check if a sequence contains only amino acids

--- gene_code_utils/amino_analyzer_utils.py
+++ gene_code_utils/amino_analyzer_utils.py
@@ -1,1 +1,17 @@
+AA_SET = set(['V', 'I', 'L', 'E', 'Q', 'D', 'N', 'H', 'W', 'F', 'Y', 'R', 'K', 'S', 'T', 'M', 'A', 'G', 'P', 'C',
+               'v', 'i', 'l', 'e', 'q', 'd', 'n', 'h', 'w', 'f', 'y', 'r', 'k', 's', 't', 'm', 'a', 'g', 'p', 'c'])
+
+
+def is_aa(seq: str) -> bool:
+    """
+    Check if a sequence contains only amino acids.
+
+    Args:
+        seq (str): The input sequence to be checked.
+
+    Returns:
+        bool: True if the sequence contains only amino acids, False otherwise.
+    """
+    unique_chars = set(seq)
+    return unique_chars <= AA_SET
 
