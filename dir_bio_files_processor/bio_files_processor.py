777f05b44bbeef93311c42bbff0af5725bea096b
CaptnClementine <131146976+CaptnClementine@users.noreply.github.com>
2023-10-16 23:44:33 +0300

    Add 'select_genes_from_gbk_to_fasta' function

--- dir_bio_files_processor/bio_files_processor.py
+++ dir_bio_files_processor/bio_files_processor.py
@@ -1,4 +1,75 @@
-def convert_multiline_fasta_to_oneline(input_fasta, output_fasta = 'output_auto.fasta'):
+from collections import deque
+
+
+def select_genes_from_gbk_to_fasta(input_gbk: str, genes: list, n_before: int, n_after: int, output_fasta: str = 'output_auto.fasta') -> None:
+    """
+    Select specific genes from a GenBank (GBK) file, extract their genetic information, and save it as a FASTA format file.
+
+    Args:
+        input_gbk (str): The path to the input GenBank file.
+        genes (list of str): A list of gene names to extract from the GenBank file.
+        n_before (int): The number of sequences to include before the selected gene.
+        n_after (int): The number of sequences to include after the selected gene.
+        output_fasta (str, optional): The name of the output FASTA file. If not provided, it will be generated based on the gene name.
+
+    Returns:
+        None
+
+    This function reads the input GenBank file, extracts CDS sequences, and selects specific genes. The selected sequences, along with surrounding context sequences, are saved as FASTA format files. Output files are named based on the gene name.
+
+    The number of sequences before and after the selected gene is specified by n_before and n_after.
+
+    """
+    with open(input_gbk) as gbk_file:
+        output_list = []
+        read_line = []
+        translation = ''
+        translation_flag = 0
+        for line in gbk_file:
+            if 'CDS ' in line:
+                read_line.append(translation)
+                output_list.append(read_line)
+                read_line = []
+                translation = ''
+                translation_flag = 0
+            elif 'gene=' in line:
+                read_line.append(line.strip())
+            elif 'translation=' in line:
+                if '"\n' in line:
+                    translation += line.strip()
+                else:
+                    translation_flag = 1
+                    translation += line.strip()
+            elif '"' not in line and translation_flag == 1:
+                translation += line.strip()
+            elif '"' in line and translation_flag == 1:
+                translation += line.strip()
+                translation_flag = 0
+        read_line.append(translation)
+        output_list.append(read_line)    
+    
+    for gene in genes:
+        output_translation = deque(["" for i in range(n_before + n_after)])
+        find_flag, counter = 0, -1
+        for cds in output_list[1:]:
+            if 'gene=' in cds[0] and gene in cds[0]:
+                find_flag = 1
+            else:
+                try:
+                    output_translation.append(cds[1].split('="')[1][:-1])
+                except:
+                    output_translation.append(cds[0].split('="')[1][:-1])
+                output_translation.popleft()
+            if find_flag == 1:
+                counter += 1
+            if counter == n_after:
+                break
+
+        with open(f'{gene}_{output_fasta}','w') as gbk_file:
+            gbk_file.write("\n".join(output_translation))
+
+
+def convert_multiline_fasta_to_oneline(input_fasta: str, output_fasta: str = None) -> None:
     """
     Convert a multiline FASTA file into a one-line FASTA format and save it to a new file.
     
@@ -19,15 +90,16 @@ def convert_multiline_fasta_to_oneline(input_fasta, output_fasta = 'output_auto.
     """
     with open(input_fasta) as fasta_file:
         output_list = []
-        s = ''
+        read_line = ''
         for line in fasta_file:
             if '>' in line:
-                output_list.append(s[:-1])
-                s = ''
+                output_list.append(read_line[:-1])
+                read_line = ''
                 output_list.append(line[:-1])
             else:
-                s += line[:-1]
-        output_list.append(s[:-1])
+                read_line += line[:-1]
+        output_list.append(read_line[:-1])
 
     with open(output_fasta,'w') as fasta_file:
-        f.write("\n".join(output_list[1:]))
+        fasta_file.write("\n".join(output_list[1:]))
+        
