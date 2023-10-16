5da5b652e4d720bf16e75a5ea478eccb194d8e6b
CaptnClementine <131146976+CaptnClementine@users.noreply.github.com>
2023-10-16 23:59:13 +0300

    Add 'change_fasta_start_pos' function to shift the start position

--- dir_bio_files_processor/bio_files_processor.py
+++ dir_bio_files_processor/bio_files_processor.py
@@ -6,7 +6,7 @@ def select_genes_from_gbk_to_fasta(input_gbk: str, genes: list, n_before: int, n
     Select specific genes from a GenBank (GBK) file, extract their genetic information, and save it as a FASTA format file.
 
     Args:
-        input_gbk (str): The path to the input GenBank file.
+        input_gbk (str): The path to the input GenBank file. Please write your path with directory etc. You can use os.path.join(dir_name, file_name) 
         genes (list of str): A list of gene names to extract from the GenBank file.
         n_before (int): The number of sequences to include before the selected gene.
         n_after (int): The number of sequences to include after the selected gene.
@@ -74,7 +74,7 @@ def convert_multiline_fasta_to_oneline(input_fasta: str, output_fasta: str = Non
     Convert a multiline FASTA file into a one-line FASTA format and save it to a new file.
     
     Args:
-        input_fasta (str): The path to the input FASTA file.
+        input_fasta (str): The path to the input FASTA file. Please write your path with directory etc. You can use os.path.join(dir_name, file_name) 
         output_fasta (str, optional): The path to the output FASTA file. If not provided, the output
         file name will be generated based on the input file name with '.fasta' extension.
     
@@ -103,3 +103,21 @@ def convert_multiline_fasta_to_oneline(input_fasta: str, output_fasta: str = Non
     with open(output_fasta,'w') as fasta_file:
         fasta_file.write("\n".join(output_list[1:]))
         
+    
+def change_fasta_start_pos(input_fasta: str, shift: int, output_fasta: str = 'output_auto.fasta') -> None:
+    """
+    Shift the start position of a FASTA sequence and save it to a new file. You can use it only for one one-line FASTA-alignment 
+
+    Args:
+        input_fasta (str): The path to the input FASTA file. Please write your path with directory etc. You can use os.path.join(dir_name, file_name) 
+        shift (int): The number of positions to shift the sequence.
+        output_fasta (str, optional): The name of the output FASTA file. If not provided, it will be generated based on the input file name.
+
+    Returns:
+        None
+    """
+    with open(input_fasta) as fasta_file:
+        data = fasta_file.readlines()
+        data[1] = data[1][shift:] + data[1][:shift]
+    with open(output_fasta,'w') as fasta_file:
+        fasta_file.write("".join(data))
