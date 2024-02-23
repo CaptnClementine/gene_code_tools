b9a0cd450f99e828f7b6b38f1045f6e9145b0026
CaptnClementine <131146976+CaptnClementine@users.noreply.github.com>
2024-02-23 21:32:43 +0300

    Add filter_dna with biopyhton functions

--- gene_code_main_operations.py
+++ gene_code_main_operations.py
@@ -1,14 +1,31 @@
-from typing import Tuple, Union, List
 import os
+from typing import Tuple, Union
+
+import numpy as np
+from Bio import SeqIO
+from Bio import SeqUtils
+
+
+class InvalidInput(ValueError):
+    """
+    Exception raised for invalid input values.
+
+    Inherits from ValueError.
+
+    Attributes:
+        message (str): Explanation of the error.
+    """
+    pass
 
 
 def filter_dna(input_path: str, output_filename: str = '', gc_bounds: Union[Tuple[int, int], int] = (0, 100),
-              length_bounds: Union[Tuple[int, int], int] = (0, 2**32), quality_threshold: int = 0) -> None:
+               length_bounds: Union[Tuple[int, int], int] = (0, 2 ** 32), quality_threshold: int = 0) -> None:
     """
     Filter and process a dictionary of FASTQ sequences based on specified criteria.
 
     Args:
-        input_path (str): Path to the input FASTQ file. Please write your path with directory etc. You can use os.path.join(dir_name, file_name) 
+        input_path (str): Path to the input FASTQ file. Please write your path with directory etc.
+                          You can use os.path.join(dir_name, file_name)
         output_filename (str): Name of the output FASTQ file. By default, it is the same as the input name.
         gc_bounds (tuple or int): GC content filtering bounds.
             If a tuple, it represents the lower and upper bounds (inclusive) for GC content as percentages.
@@ -22,7 +39,7 @@ def filter_dna(input_path: str, output_filename: str = '', gc_bounds: Union[Tupl
             Default is 0 (phred33 scale).
 
     Returns:
-        filtered_seqs (dict): A dictionary containing filtered FASTQ sequences.
+        filtered_seqs (dict): A file containing filtered FASTQ sequences.
             Key: Sequence name (string).
             Value: Tuple of two strings (sequence, comment, quality).
 
@@ -32,22 +49,68 @@ def filter_dna(input_path: str, output_filename: str = '', gc_bounds: Union[Tupl
           quality_threshold will be 30 <= sequence score
 
     """
-    seqs = read_fastq_file(input_path)
-    filtered_seqs = dict()
-    for fastq_name, (seq, seq_comment, quality) in seqs.items():
-        if is_dna(seq):
-            if type(gc_bounds) == int:
-                gc_bounds = (0, gc_bounds)
-            if type(length_bounds) == int:
-                length_bounds = (0, length_bounds)
-            if is_in_gc_bounds(gc_bounds, seq) and is_in_length_bounds(length_bounds, seq) and check_quality(quality_threshold, quality):
-                filtered_seqs[fastq_name] = (seq,seq_comment, quality)
-    
+
+    good_reads = []
+    records = SeqIO.parse(input_path, "fastq")
+    if isinstance(gc_bounds, int):
+        gc_bounds = (0, gc_bounds)
+    if isinstance(length_bounds, int):
+        length_bounds = (0, length_bounds)
+    if not isinstance(gc_bounds, tuple) or not isinstance(length_bounds, tuple):
+        raise InvalidInput()
+    for record in records:
+        if (np.mean(record.letter_annotations["phred_quality"]) >= quality_threshold) and \
+                is_in_gc_bounds(gc_bounds, SeqUtils.gc_fraction(record) * 100) and \
+                is_in_length_bounds(length_bounds, len(record.seq)):
+            good_reads.append(record)
+
     if output_filename == '':
-        output_filename =  os.path.basename(input_path)
+        output_filename = os.path.basename(input_path)
     if not os.path.exists('fastq_filtrator_results'):
         os.mkdir('fastq_filtrator_results')
     output_filename = os.path.join('fastq_filtrator_results', output_filename)
-    write_filtered_fastq(filtered_seqs, output_filename)
+    SeqIO.write(good_reads, handle=output_filename, format="fastq")
 
     return None
+
+
+def is_in_gc_bounds(bounds: tuple, gc_content: float) -> bool:
+    """
+    Check if the GC content of a DNA sequence falls within the specified bounds.
+
+    Args:
+        bounds (tuple): A tuple specifying the lower and upper bounds for GC content.
+        gc_content (float): GC content of input DNA.
+
+    Returns:
+        bool: True if the GC content is within the bounds, False otherwise.
+    """
+
+    upper_bound = max(bounds[0], bounds[1])
+    lower_bound = min(bounds[0], bounds[1])
+    if upper_bound <= 0 or lower_bound < 0:
+        raise InvalidInput("Invalid gc_bounds. Each value must be greater than zero.")
+    if upper_bound == lower_bound:
+        upper_bound += 1
+    return lower_bound <= gc_content <= upper_bound
+
+
+def is_in_length_bounds(bounds: tuple, dna_length: int) -> bool:
+    """
+    Check if the length of a DNA sequence falls within the specified bounds.
+
+    Args:
+        bounds (tuple): A tuple specifying the lower and upper bounds for sequence length.
+        dna_length (int): The length of input DNA sequence.
+
+    Returns:
+        bool: True if the sequence length is within the bounds, False otherwise.
+    """
+
+    lower_bound = min(bounds[0], bounds[1])
+    upper_bound = max(bounds[0], bounds[1])
+    if upper_bound < 0 or lower_bound < 0:
+        raise ValueError("Invalid length_bounds. Each value must be greater than zero.")
+    if upper_bound == lower_bound:
+        upper_bound += 1
+    return lower_bound <= dna_length <= upper_bound
